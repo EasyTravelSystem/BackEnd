a30b194ad7e8a59af3986a9182c5be654f87ddeb
Dilan Madhusanka <dilan.madhusanka289@gmail.com>
2021-05-14 19:47:48 +0530

    create profile

--- src/main/java/com/designprojectteam/easytravelling/controller/TestController.java
+++ src/main/java/com/designprojectteam/easytravelling/controller/TestController.java
@@ -1,15 +1,40 @@
 package com.designprojectteam.easytravelling.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.designprojectteam.easytravelling.helper.Complains;
+import com.designprojectteam.easytravelling.models.ComplainUser;
+import com.designprojectteam.easytravelling.models.Profile;
+import com.designprojectteam.easytravelling.models.User;
+import com.designprojectteam.easytravelling.payload.response.MessageResponse;
+import com.designprojectteam.easytravelling.repository.ComplainRepository;
+import com.designprojectteam.easytravelling.repository.ProfileRepository;
+import com.designprojectteam.easytravelling.repository.UserRepository;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/test")
 public class TestController {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Autowired
+	ProfileRepository profileRepository;
+	
+	@Autowired
+	ComplainRepository complainRepositroy;
 
 	@GetMapping("/all")
 	public String allAccess() {
@@ -33,4 +58,6 @@ public class TestController {
 	public String adminAccess() {
 		return "Admin Board.";
 	}
+
+	
 }
--- src/main/java/com/designprojectteam/easytravelling/repository/ProfileRepository.java
+++ src/main/java/com/designprojectteam/easytravelling/repository/ProfileRepository.java
@@ -1,9 +1,15 @@
 package com.designprojectteam.easytravelling.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.designprojectteam.easytravelling.models.Profile;
 
 public interface ProfileRepository extends MongoRepository<Profile, String> {
+
+	Optional<Profile> findByUserId(String userId);
+
+	boolean existsByUserId(String userId);
 
 }
