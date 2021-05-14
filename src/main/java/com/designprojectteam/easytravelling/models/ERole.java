9fcccacb31225d18b1fe225ef7ccfc580236ff53
Dilan Madhusanka <dilan.madhusanka289@gmail.com>
2021-05-14 20:24:02 +0530

    remove all code

--- src/main/java/com/designprojectteam/easytravelling/models/ERole.java
+++
@@ -1,7 +0,0 @@
-package com.designprojectteam.easytravelling.models;
-
-public enum ERole {
-	ROLE_USER,
-	ROLE_MODERATOR,
-	ROLE_ADMIN
-}
--- src/main/java/com/designprojectteam/easytravelling/repository/ComplainRepository.java
+++
@@ -1,9 +0,0 @@
-package com.designprojectteam.easytravelling.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.designprojectteam.easytravelling.models.ComplainUser;
-
-public interface ComplainRepository extends MongoRepository<ComplainUser, String> {
-
-}
